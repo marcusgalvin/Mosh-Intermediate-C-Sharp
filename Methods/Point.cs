26fe9a29b41b98903247fc89703bba7823ce9646
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-01-28 13:18:04 -0500

    Initial check-in of module Methods

---
+++ Methods/Point.cs
@@ -0,0 +1,27 @@
+﻿namespace Methods
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void Move(int x, int y)
+        {
+            this.x = y;
+            this.x = x;
+        }
+
+        public void Move (Point newLocation)
+        {
+            Move(newLocation.x, newLocation.y);
+           
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("point is at {0},{1}", point.x, point.y);
+
+            point.Move(100, 200);
+            Console.WriteLine("point is at {0}, {1}", point.x, point.y);
+        }
+    }
+}
